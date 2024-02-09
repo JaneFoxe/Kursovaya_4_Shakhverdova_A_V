15db61e95e66861e39147a70414aeb3b771a8107
JaneFoxe <Kukusiki2204@gmail.com>
2024-02-09 22:31:18 +0300

    Создала абстрактный метод AbstractAPI(ABC)

--- src/Classes.py
+++ src/Classes.py
@@ -1,1 +1,13 @@
+from abc import ABC, abstractmethod
+import requests
 
+
+class AbstractAPI(ABC):
+
+    @abstractmethod
+    def get_vacancies(self, text):
+        pass
+
+    @abstractmethod
+    def filter_vacancies(self, text):
+        pass
