6ca21b2c07ba7811e2f11664671df379b4f618bb
JaneFoxe <Kukusiki2204@gmail.com>
2024-02-10 00:24:39 +0300

    Класс Vacancy

---
+++ src/Vacancy.py
@@ -0,0 +1,10 @@
+
+
+class Vacancy:
+    def __int__(self, name, url, salary_from, salary_to, description):
+        self.name = name
+        self.url = url
+        self.salary_from = salary_from
+        self.salary_to = salary_to
+        self.description = description
+
