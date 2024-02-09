3007cc9e70eb81b094b6e87f723689d4ddfbc4aa
JaneFoxe <Kukusiki2204@gmail.com>
2024-02-10 01:09:16 +0300

    update

--- src/Vacancy.py
+++ src/Vacancy.py
@@ -1,7 +1,7 @@
 
 
 class Vacancy:
-    def __int__(self, name, url, salary_from, salary_to, description):
+    def __init__(self, name, url, salary_from, salary_to, description):
         self.name = name
         self.url = url
         self.salary_from = salary_from
